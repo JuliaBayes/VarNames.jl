57d1425214d7b83afa50653f709f5f720831c927
Penelope Yong <penelopeysm@gmail.com>
2026-08-20 01:30:05 +0100

    Format

--- test/draw.jl
+++ test/draw.jl
@@ -5,8 +5,8 @@ using Test
 
 @testset "draw" begin
     # Since it's just a bare struct, just test its API.
-    vnt = VarNamedTuple(x = 0.5, y = randn(3))
-    nt = (a = :hello, logjoint = randn())
+    vnt = VarNamedTuple(x=0.5, y=randn(3))
+    nt = (a=:hello, logjoint=randn())
     d = Draw(vnt, nt)
 
     @test parameters(d) == vnt
